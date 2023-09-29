fde82d61e1da183193e753513e30594115e0ff6b
fedepaloz <palozzifederico@gmail.com>
2023-09-29 15:42:49 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+const tip = [
+  {
+    text: "bad",
+    percentage: 0,
+  },
+  {
+    text: "good",
+    percentage: 10,
+  },
+  {
+    text: "excellent",
+    percentage: 10,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BillInput></BillInput>
+      <ServiceInput>How did you like the service?</ServiceInput>
+      <ServiceInput>How did your friend like the service?</ServiceInput>
     </div>
   );
 }
 
 export default App;
+
+function BillInput() {
+  return (
+    <div>
+      <span>How much was the bill?</span>
+      <input type="number"></input>
+    </div>
+  );
+}
+function ServiceInput({ children }) {
+  return (
+    <div>
+      <span>{children}</span>
+      <option></option>
+    </div>
+  );
+}
